45d36ef26a9acbf638633baa13048094bc6b2553
who885 <who885@ymail.com>
2022-04-25 23:46:44 -0400

    added elements to the home page and some css styling.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     require 'Classes\Database.php';
     $conn = require  'includes\db.php';
     $articles = Music::getByID($conn, 1);
+    $articles2 = Music::getByID($conn, 2);
     $lyrics = Lyrics::getByID($conn, 1);
     $dates = Dates::getByID($conn, 1);
     $translated = Translated::getByID($conn, 1);
@@ -27,10 +28,23 @@
         <li><a href="#">Page2</a></li>
         <li><a href="#">Page3</a></li>
       </ul>
+      <div class="Shadow">
+        <img class="head_img" src="<?php echo $articles->thumb_link; ?>" alt="">
+        </div>
     </nav>
 
 
-    <img src="<?php echo $articles->thumb_link; ?>" alt="">
+
+<div class="article_1">
+    <img class="art_1_img" src="<?php echo $articles2->thumb_link; ?>" alt="">
+    <div class="center">
+    <h2>Article_1</h2>
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Cras sed nulla pretium, placerat nisl sed, ornare ipsum. Ut et eros eu tellus </p>
+    </div>
+    </div>
+</div>
+
 
 
   </body>
