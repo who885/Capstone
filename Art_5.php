3233c1b9c1b5e67468e5139092c45fcd67fe151d
who885 <who885@ymail.com>
2022-05-05 12:40:27 -0400

    added jake's article.

--- Art_5.php
+++ Art_5.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-  <link rel="stylesheet" href="/css/article.css">
+    <link rel="stylesheet" href="/css/article.css">
     <?php
-    require 'Classes\Music.php';
-    require 'Classes\Lyrics.php';
-    require 'Classes\Translated.php';
-    require 'Classes\Dates.php';
-    require 'Classes\Database.php';
-    $conn = require  'includes\db.php';
+    require 'Classes/Music.php';
+    require 'Classes/Lyrics.php';
+    require 'Classes/Translated.php';
+    require 'Classes/Dates.php';
+    require 'Classes/Database.php';
+    $conn = require  'includes/db.php';
 //$conn needs to be in there, the number corresponds to the id number in the tables.
 
-$articles = Music::getByID($conn, 4);
-$lyrics = lyrics::getByID($conn, 10);
+$articles = Music::getByID($conn, 3);
+$lyrics = lyrics::getByID($conn, 8);
 $translate = Translated::getByID($conn,1);
      ?>
     <meta charset="utf-8">
@@ -53,19 +53,20 @@ $translate = Translated::getByID($conn,1);
 </div>
 
     <div class="mainimage">
-    <img class="Main Image" src="<?php echo $articles->thumb_link; ?>" alt="">
+    <img class="Main Image" src="<?php echo $articles->thumb_link; ?>" alt="" >
 </div>
 
     <div class="description">
-    <b>Title</b>
-    <p>"Ut sodales, sapien non sagittis rutrum, lorem ante placerat est, id porttitor enim diam at dui. Cras laoreet dapibus dolor a auctor. Morbi sollicitudin, nisl nec fermentum condimentum, odio ex rutrum arcu, quis dapibus mi massa ut lacus. Suspendisse a scelerisque leo. Suspendisse vel eros sed dolor volutpat bibendum. Quisque blandit gravida libero, eu tincidunt massa cursus non. Donec euismod, dui in facilisis fringilla, velit felis consequat enim, ut fringilla nunc turpis vitae nibh. Nam tristique justo pellentesque vehicula fringilla. Mauris sed consequat ligula. Quisque dui erat, laoreet ac lobortis non, finibus vel purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis volutpat eros a purus venenatis, sit amet egestas quam lobortis. Donec viverra scelerisque augue a dictum. Nunc volutpat ultrices nisi non tristique." </p>
+    <b>I Break Mirrors With My Face in the United States</b>
+    <p>Death Grips are a music collective based out of Sacramento California. They make avant garde electronic music, which usually includes the use of noise. 'I Break Mirrors With My Face' is such a great song, simply due to the amount of energy the lead singer, MC Ride, gives off. This is only accentuated by the gritty production provided by Zach Hill and Andy Morin.
+ </p>
   </div>
 
   <div class="lyrics">
     <?php $str = $lyrics->lyrics; ?>
   <span>
-  <a href="http://www.<?php echo $str ?>"><img src="images/genius.png" style = "display: block; width: 200px; margin-left: auto;
-  margin-right: auto;"></a>
+    <a href="http://www.<?php echo $str ?>"><img src="images/genius.png" style = "display: block; width: 200px; margin-left: auto;
+    margin-right: auto;"></a>
 </span>
 </div>
 
